c35c4195937cfd46e847dd09bbfd51fcbd41de4c
mertje <mert34gunes@gmail.com>
2022-08-26 16:26:39 +0200

    Added reset for keyboard

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
-for tw: yarn tailwindcss -i ./src/input.css -o ./dist/browserstyles.css --watch
-start: yarn dev
+Wanted to play some Wordle but it was once a day so i created a one i can play as much as posible
+
+yarn tailwindcss -i ./src/input.css -o ./dist/browserstyles.css --watch
+yarn dev
--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,143 @@
-import { Words } from './words'
+import { Words } from "./words";
 
-endgame()
+endgame();
 function endgame() {
-    const infoID = document.getElementById('information') as HTMLDivElement;
+    const infoID = document.getElementById("information") as HTMLDivElement;
     const startOver = document.createElement("div");
-    startOver.className = 'px-4 py-1 rounded bg-lime-600 text-white';
-    startOver.innerText = 'Start Game';
+    startOver.className = "px-4 py-1 rounded bg-lime-600 text-white";
+    startOver.innerText = "Start Game";
     infoID.appendChild(startOver);
 
-    startOver.addEventListener('click', () => {
-        infoID.removeChild(startOver)
-        initGame()
-    })
+    clearKeyboard();
+
+    startOver.addEventListener("click", () => {
+        infoID.removeChild(startOver);
+        initGame();
+    });
 }
 
 function initGame() {
-    const playBoard = document.getElementById('play-board') as HTMLDivElement
-    playBoard.innerHTML = ''
+    const playBoard = document.getElementById("play-board") as HTMLDivElement;
+    playBoard.innerHTML = "";
 
     // creating the play board
     for (let i = 0; i < 6; i++) {
         const row = document.createElement("div");
-        row!.className = 'box-rows flex';
+        row!.className = "box-rows flex";
         playBoard!.prepend(row);
 
         for (let i = 0; i < 5; i++) {
             const input = document.createElement("div");
-            input!.className = 'box uppercase font-bold text-4xl m-0.5 rounded border border-gray-500 h-12 w-12 flex justify-center items-center duration-500 delay-200';
-            row.prepend(input)
+            input!.className =
+                "box uppercase font-bold text-4xl m-0.5 rounded border border-gray-500 h-12 w-12 flex justify-center items-center duration-500 delay-200";
+            row.prepend(input);
         }
     }
-    listenInput()
+    listenInput();
 }
 
 function listenInput() {
-    const trysAt = { 'attempt': 0 }
+    const trysAt = { attempt: 0 };
     const userWord: string[] = [];
     const guesThis = randomWord(Words);
 
-    document.addEventListener('keyup', (event) => {
-        checkInput({ userWord, guesThis, event, trysAt })
-        if (trysAt.attempt > 5) return endgame()
-    })
+    document.addEventListener("keyup", (event) => {
+        checkInput({ userWord, guesThis, event, trysAt });
 
-
+        if (trysAt.attempt > 5) setTimeout(() => endgame(), 2000);
+    });
 }
 
 type checker = {
-    userWord: string[],
-    guesThis: string[],
-    event: KeyboardEvent,
-    trysAt: { "attempt": number },
-}
+    userWord: string[];
+    guesThis: string[];
+    event: KeyboardEvent;
+    trysAt: { attempt: number };
+};
 
 function checkInput({ userWord, guesThis, event, trysAt }: checker) {
-    let currentRow = document.getElementsByClassName('box-rows')[trysAt.attempt];
+    let currentRow = document.getElementsByClassName("box-rows")[trysAt.attempt];
 
-    if (userWord.length < 5 && event.key.match(/[a-z]/gi) && event.key.length < 2) {
-        userWord.push(event.key.toString())
-        currentRow.children[userWord.length - 1].innerHTML = userWord[userWord.length - 1]
+    if (
+        userWord.length < 5 &&
+        event.key.match(/[a-z]/gi) &&
+        event.key.length < 2
+    ) {
+        userWord.push(event.key.toString());
+        currentRow.children[userWord.length - 1].innerHTML =
+            userWord[userWord.length - 1];
     }
-    if (event.key === 'Backspace' && userWord.length !== 0) {
-        currentRow.children[userWord.length - 1].innerHTML = ''
-        userWord.splice(-1)
+    if (event.key === "Backspace" && userWord.length !== 0) {
+        currentRow.children[userWord.length - 1].innerHTML = "";
+        userWord.splice(-1);
     }
-    if (event.key === 'Enter' && userWord.length === 5) {
-        if (Words.includes(userWord.join(''))) {
+    if (event.key === "Enter" && userWord.length === 5) {
+        if (Words.includes(userWord.join(""))) {
             userWord.forEach((uword: string, index: number) => {
                 setTimeout(() => {
-                    if (guesThis.join('').includes(uword)) {
+                    if (guesThis.join("").includes(uword)) {
                         if (guesThis[index] === uword) {
-                            currentRow.children[index].classList.add('bg-green-500', 'text-white')
-                            keyboardColor('bg-green-500', uword);
+                            currentRow.children[index].classList.add(
+                                "bg-green-500",
+                                "text-white"
+                            );
+                            keyboardColor("bg-green-500", uword);
                         } else {
-                            currentRow.children[index].classList.add('bg-amber-500', 'text-white')
-                            keyboardColor('bg-amber-500', uword);
+                            currentRow.children[index].classList.add(
+                                "bg-amber-500",
+                                "text-white"
+                            );
+                            keyboardColor("bg-amber-500", uword);
                         }
                     } else {
-                        currentRow.children[index].classList.add('bg-gray-400', 'text-white')
-                        keyboardColor('bg-gray-400', uword);
+                        currentRow.children[index].classList.add(
+                            "bg-gray-400",
+                            "text-white"
+                        );
+                        keyboardColor("bg-gray-400", uword);
                     }
                 }, index * 400);
-            })
-            if (userWord.join('') === guesThis.join('')) return endgame()
-            trysAt.attempt++
-            userWord.length = 0
+            });
+            if (userWord.join("") === guesThis.join("")) setTimeout(() => endgame(), 2000);
+            trysAt.attempt++;
+            userWord.length = 0;
         } else {
-            const infoID = document.getElementById('information') as HTMLDivElement;
+            const infoID = document.getElementById("information") as HTMLDivElement;
             const notExist = document.createElement("div");
-            notExist.className = 'px-4 py-1 rounded bg-cyan-400 text-white';
-            notExist.innerText = 'you fucked up';
+            notExist.className = "px-4 py-1 rounded bg-cyan-400 text-white";
+            notExist.innerText = "you fucked up";
             infoID.appendChild(notExist);
 
             setTimeout(() => {
-                infoID.removeChild(notExist)
+                infoID.removeChild(notExist);
             }, 2500);
+        }
+    }
+}
+
+function clearKeyboard() {
+    const letterbuttons = document.getElementsByTagName("button");
+    for (let button of letterbuttons) {
+        if (!button.className.includes("bg-gray-300")) {
+            button.className.replace(/(bg-.*?00)/g, "bg-gray-300");
+            const colorButton = button.className.match(
+                /(bg-.*?00)/g
+            ) as RegExpMatchArray;
+
+            button.classList.replace(colorButton[0], "bg-gray-300");
         }
     }
 }
 
 function keyboardColor(color: string, pressedLetter: string) {
     const button = document.getElementsByClassName(pressedLetter);
-    if (button[0].classList.contains('bg-gray-300')) {
-        button[0].classList.remove('bg-gray-500')
-        button[0].classList.add(color)
+    if (button[0].classList.contains("bg-gray-300")) {
+        button[0].classList.remove("bg-gray-300");
+        button[0].classList.add(color);
     }
 }
 
-
 function randomWord(wordArray: string[]): string[] {
-    const chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)]
-    return chosenWord.split('')
+    const chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    return chosenWord.split("");
 }
